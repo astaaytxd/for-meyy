4b8a4d067c775c6c3b6c9d855652b8a89ef1e103
AstaYT <125131061+astaaytxd@users.noreply.github.com>
2023-05-27 05:14:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+window.addEventListener('load', () => {
+
+    Swal.fire({
+
+        title: 'Do you want to play music in the background?',
+
+        // text: "You won't be able to revert this!",
+
+        icon: 'warning',
+
+        showCancelButton: true,
+
+        confirmButtonColor: '#3085d6',
+
+        cancelButtonColor: '#d33',
+
+        confirmButtonText: 'Yes',
+
+        cancelButtonText: 'No',
+
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+
+            document.querySelector('.song').play();
+
+            animationTimeline();
+
+        } else {
+
+            animationTimeline();
+
+        }
+
+    });
+
+});
+
+
+
 const quoteArea = document.querySelector('.quote-area');
 
 const btnLanjutQ = document.querySelector('.btn2');
